cd9ff06ae1c041cc94fd987091f85d42c1fe9675
Eric <eric@ericmaicon.com.br>
2013-06-18 10:05:36 -0300

    init

---
+++ rf.c
@@ -0,0 +1,15 @@
+/*
+http://www.seeedstudio.com/forum/viewtopic.php?f=4&t=309&p=767&hilit=pt2262#p767
+http://playground.arduino.cc/Portugues/LearningSoftwareSerial
+http://blogdonatanael.blogspot.com.br/2011/12/criando-um-controle-remoto-simples-para.html
+http://blog.generaleccentric.net/?tag=holtek
+http://www.raspberrypi.org/phpBB3/viewtopic.php?f=33&t=11303
+http://www.raspberrypi.org/phpBB3/viewtopic.php?t=24215&p=239410
+http://www.botskool.com/tutorials/electronics/general-electronics/building-rf-remote-control
+http://www.engineersgarage.com/electronic-circuits/wireless-rf-remote-control-circuit
+http://ashishrd.blogspot.com.br/2011/05/building-computer-controlled-wireless.html
+http://electronicsviaweb.blogspot.com.br/2012/07/arduino-project.html
+
+compilar: gcc rf.c -lwiringPi -Werror -o rf
+executar: sudo ./rf
+*/
